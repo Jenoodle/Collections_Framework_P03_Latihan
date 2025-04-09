63d5cc4550d8cfaebf311ddd3f4d917b7f5e7d75
Jenie J <162806158+Jenoodle@users.noreply.github.com>
2025-04-09 17:48:21 +0700

    Add files via upload
    MavenProject8

---
+++ MavenProject8/src/mavenproject8/MavenProject8.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mavenproject8;
+
+import java.util.*;
+    public class MavenProject8 {
+        public static void main(String[] args) {
+            
+            Vector v = new Vector();
+            
+            //add elements to Vector
+            v.add ("A");
+            v.add ("B");
+            v.add ("C");
+            v.add ("D");
+            v.add ("E");
+            v.add ("F");
+            
+            system.out.println("Vector contains : " + v);
+            Enumeration e = v.elements();
+            system.out.println("Arraylist contains : " + alist);
+        }
+    }
