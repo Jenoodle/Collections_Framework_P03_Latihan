f7a5b37c99adbbaa813e98b0faa467a84ca83dc7
Jenie J <162806158+Jenoodle@users.noreply.github.com>
2025-04-09 17:47:06 +0700

    Add files via upload
    MavenProject5

---
+++ MavenProject5/src/mavenproject5/MavenProject5.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mavenproject5;
+
+import java.util.*;
+
+public class MavenProject5 {
+    public static void main(String[] args) {
+        List list=new ArrayList();
+             list.add("Jenie");
+             list.add("Sherly");
+             list.add("Nopeliba");
+             list.add("Oktria");
+             list.add("Angelyne");
+             list.add("Michella");
+             System.out.println(list);
+             System.out.println("2 : "+list.get(2));
+             System.out.println("0 : "+list.get(0));
+             LinkedList queue = new LinkedList();
+             queue.addFirst("Oktria");
+             queue.addFirst("Nopeliba");
+             queue.addFirst("Jenie");
+             queue.addFirst("Michella");
+             queue.addFirst("Sherly");
+             queue.addFirst("Angelyne");
+             System.out.println(queue);
+             queue.removeLast();
+             System.out.println(queue);
+    }
+        }
+
