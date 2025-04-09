bae3d062d8b3850e99f15a86d0edf634e557d5f3
Jenie J <162806158+Jenoodle@users.noreply.github.com>
2025-04-09 17:47:30 +0700

    Add files via upload
    MavenProject6

---
+++ MavenProject6/src/mavenproject6/MavenProject6.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mavenproject6;
+
+import java.util.*;
+
+public class MavenProject6 {
+    public static void main(String[] args) {
+        
+        Vector<String> vc=new Vector<String>();
+
+        vc.add("Vector Object 1");
+        vc.add("Vector Object 2");
+        vc.add("Vector Object 3");
+        vc.add("Vector Object 4");
+        vc.add("Vector Object 5");
+
+        vc.add(3, "Element at fix position");
+
+        System.out.println("Vector Size :"+vc.size());
+
+        for(int i=0;i<vc.size();i++)
+      {
+        System.out.println("Vector Element "+i+" :"+vc.get(i));
+    }
+  }
+}
